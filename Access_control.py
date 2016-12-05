03b0dccc57ff03a6b9956e32fa9c976080ca7cef
Unknown <Saurabh.shendye@gmail.com>
2016-12-05 01:41:07 -0700

    With json data parsing

--- Access_control.py
+++ Access_control.py
@@ -12,207 +12,219 @@ from ryu.lib.packet import arp
 from ryu.lib.packet import udp
 from ryu.lib.packet import tcp
 
+
 class Access_control(app_manager.RyuApp):
-	OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-	
-	def __init__(self, *args, **kwargs):
-		"This is counstructor"
-		super(Access_control, self).__init__(*args, **kwargs)
-		self.mac_to_port = {}
-		self.IP_to_mac = {}
-		print("First RYU APP")
-	
-	@set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
-	def switch_features_handler(self, ev):
-		"To add the table flow miss entry"
-		datapath = ev.msg.datapath
-		ofproto = datapath.ofproto
-		parser = datapath.ofproto_parser
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
-		match = parser.OFPMatch()
-		actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, 
-						  ofproto.OFPCML_NO_BUFFER)]
-		self.add_flow(datapath, 0, match, actions)
+    def __init__(self, *args, **kwargs):
+        "This is counstructor"
+        super(Access_control, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        self.IP_to_mac = {}
+        print("First RYU APP")
 
-	def add_flow(self, datapath, priority, match, actions):
-		"This is to add/send flows to switch"		
-		ofproto = datapath.ofproto
-		parser = datapath.ofproto_parser
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        "To add the table flow miss entry"
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
 
-		# To change/modify the flow entries in the switch
-		inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-						     actions)]
-		if priority == 0:
-			mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-						match=match, instructions=inst)
-		else:
-			mod = parser.OFPFlowMod(datapath=datapath,priority= priority,
-						idle_timeout= 10, hard_timeout= 20,
-						match=match, instructions=inst)
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
 
-		datapath.send_msg(mod)
+    def add_flow(self, datapath, priority, match, actions):
+        "This is to add/send flows to switch"
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
 
-	def allow_access(self,pkt, msg):
-		"To allow Access for communication"
-		datapath = msg.datapath
-		ofproto = datapath.ofproto
-		parser = datapath.ofproto_parser
-		dpid = datapath.id
-		self.mac_to_port.setdefault(dpid, {})		
-		
-		eth_packet = pkt.get_protocol(ethernet.ethernet)
-		src = eth_packet.src
-		dst = eth_packet.dst
-		arp_packet = pkt.get_protocol(arp.arp)
-		ipv4_packet = pkt.get_protocol(ipv4.ipv4)
+        # To change/modify the flow entries in the switch
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if priority == 0:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    idle_timeout=10, hard_timeout=20,
+                                    match=match, instructions=inst)
 
-		if ipv4_packet:
-			source_ip = ipv4_packet.src
-			destination_ip = ipv4_packet.dst
-		elif arp_packet:
-			source_ip = arp_packet.src_ip
-			destination_ip = arp_packet.dst_ip
-		
-		self.IP_to_mac[source_ip] = src
-		self.IP_to_mac[destination_ip] = dst
+        datapath.send_msg(mod)
 
-		print(self.IP_to_mac)			
+    def allow_access(self, pkt, msg):
+        "To allow Access for communication"
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
 
-		in_port = msg.match['in_port']
-		self.mac_to_port[dpid][src] = in_port
-		
-		if dst in self.mac_to_port[dpid]:
-			out_port = self.mac_to_port[dpid][dst]
-		else:
-			out_port = ofproto.OFPP_FLOOD
-		
-		actions = [parser.OFPActionOutput(out_port)]		
+        eth_packet = pkt.get_protocol(ethernet.ethernet)
+        src = eth_packet.src
+        dst = eth_packet.dst
+        arp_packet = pkt.get_protocol(arp.arp)
+        ipv4_packet = pkt.get_protocol(ipv4.ipv4)
 
-		if out_port != ofproto.OFPP_FLOOD:
-			match = parser.OFPMatch(in_port = in_port, eth_dst = dst)
-			self.add_flow(datapath, 1, match, actions)
-		
-		out = parser.OFPPacketOut(datapath = datapath, 
-					  buffer_id = ofproto.OFP_NO_BUFFER,
-					  in_port = in_port, actions = actions,
-					  data = msg.data)
-		
-		datapath.send_msg(out)
-					  	
-	def deny_access(self, pkt, msg):
-		"To deny access for communication"
-		datapath = msg.datapath
-		ofproto = datapath.ofproto
-		parser = datapath.ofproto_parser
-		dpid = datapath.id
-		
-		eth_packet = pkt.get_protocol(ethernet.ethernet)
-		arp_packet = pkt.get_protocol(arp.arp)
-		ipv4_packet = pkt.get_protocol(ipv4.ipv4)
-		#print("IPv4 Packet: ", ipv4_packet)
-		#print("ARP src type: ",type(arp_packet.src_ip))
-		
-		source = arp_packet.src_ip
-		dest = arp_packet.dst_ip
-		src = eth_packet.src
-		dst = eth_packet.dst
+        if ipv4_packet:
+            source_ip = ipv4_packet.src
+            destination_ip = ipv4_packet.dst
+        elif arp_packet:
+            source_ip = arp_packet.src_ip
+            destination_ip = arp_packet.dst_ip
 
-		self.IP_to_mac[source] = src		
-		print(self.IP_to_mac)
-		in_port = msg.match['in_port']
+        self.IP_to_mac[source_ip] = src
+        self.IP_to_mac[destination_ip] = dst
 
-		# match = parser.OFPMatch(arp_spa = source, arp_tpa = dest)
-		if dest in self.IP_to_mac.keys():
-			match = parser.OFPMatch(eth_src = src, eth_dst = self.IP_to_mac[dest])
-		else:
-			match = parser.OFPMatch(eth_src = src, eth_dst = dst)
+        print(self.IP_to_mac)
 
-		# print("Match: ",  match)
-		mod = parser.OFPFlowMod(datapath=datapath, priority=1,
-					idle_timeout=10, hard_timeout=20,
-					match=match, instructions=[])
-		# print("Mod: ", mod)
-		
-		# out = parser.OFPPacketOut(datapath = datapath,
-		#			  buffer_id = ofproto.OFP_NO_BUFFER,
-		#			  data = msg.data, in_port = in_port,
-		#			  actions)
+        in_port = msg.match['in_port']
+        self.mac_to_port[dpid][src] = in_port
 
-		datapath.send_msg(mod)
-		
-	
-	@set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-	def packet_in_handler(self, ev):
-		"When a packet_in message is received policy is enforced"
-		print("Packet In Message received")
-		packet_restriction = 1
-		group_restriction = 1
-		G1 = ["10.10.1.1", "10.10.1.2"]
-		G2 = ["10.10.1.3"]
-		
-		msg = ev.msg
-		datapath = msg.datapath
-		ofproto = datapath.ofproto
-		parser = datapath.ofproto_parser
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
 
-		# dpid = datapath.id
-		
-		pkt = packet.Packet(msg.data)
-		# print(pkt.protocols)
-		arp_packet = pkt.get_protocols(arp.arp)
-		ipv4_packet = pkt.get_protocols(ipv4.ipv4)
-		udp_packet = pkt.get_protocols(udp.udp)
-		if udp_packet:
-			if packet_restriction == 1:
-				print("UDP packet detected. Access Denied")
-				return
-			else:
-				pass
+        actions = [parser.OFPActionOutput(out_port)]
 
-		if arp_packet:
-			print(arp_packet)
-			source_ip = arp_packet[0].src_ip
-			destination_ip = arp_packet[0].dst_ip
-		elif ipv4_packet:
-			# print("IPV4 Packet: ", ipv4_packet)
-			source_ip = ipv4_packet[0].src
-			destination_ip = ipv4_packet[0].dst
-			# print("Source: ", source_ip)
-			# print("Destiation: ", destination_ip)
-		
-		if source_ip in G1:
-			src_g = 1
-		elif source_ip in G2:
-			src_g = 2
-		else:
-			src_g = 0
-		
-		if destination_ip in G1:
-			dest_g = 1
-		elif destination_ip in G2:
-			dest_g = 2
-		else:
-			dest_g = 0
-		
-		if group_restriction == 1:
-			if dest_g == src_g:
-				print("Allow Packet Transfer")
-				self.allow_access(pkt, msg)
-			else:
-				print("Deny the Access")
-				self.deny_access(pkt, msg)
-		else:
-			print("Allow Access")
-			self.allow_access(pkt, msg)
-			
-		
-		
-		
-		
-		
-		
-		
-		
-		
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            self.add_flow(datapath, 1, match, actions)
 
-		
+        out = parser.OFPPacketOut(datapath=datapath,
+                                  buffer_id=ofproto.OFP_NO_BUFFER,
+                                  in_port=in_port, actions=actions,
+                                  data=msg.data)
+
+        datapath.send_msg(out)
+
+    def deny_access(self, pkt, msg):
+        "To deny access for communication"
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        dpid = datapath.id
+
+        eth_packet = pkt.get_protocol(ethernet.ethernet)
+        arp_packet = pkt.get_protocol(arp.arp)
+        ipv4_packet = pkt.get_protocol(ipv4.ipv4)
+
+        source = arp_packet.src_ip
+        dest = arp_packet.dst_ip
+        src = eth_packet.src
+        dst = eth_packet.dst
+
+        self.IP_to_mac[source] = src
+        print(self.IP_to_mac)
+        in_port = msg.match['in_port']
+
+        if dest in self.IP_to_mac.keys():
+            match = parser.OFPMatch(eth_src=src, eth_dst=self.IP_to_mac[dest])
+        else:
+            match = parser.OFPMatch(eth_src=src, eth_dst=dst)
+
+        mod = parser.OFPFlowMod(datapath=datapath, priority=1,
+                                idle_timeout=10, hard_timeout=20,
+                                match=match, instructions=[])
+
+        datapath.send_msg(mod)
+
+    def jparsing(self):
+        import json
+
+        global packet_restriction
+        global group_restriction
+
+        # data = '{"Policy": [{"group_isolation": "0", "UDP_packet_restriction": "0"}], "Groups": [{"1": {"host_ips": ["10.10.1.1", "10.10.1.2", "10.10.1.3"]}, "2": {"host_ips": ["10.10.1.4"]}}]}'
+        f = open('Received.txt', 'r')
+        data = f.read()
+        f.close()
+
+        parsed_json = json.loads(data)
+        number_of_grps = len(parsed_json["Groups"][0])
+        grp_name_list = parsed_json["Groups"][0].keys()
+        group_restriction = int(parsed_json['Policy'][0]['group_isolation'])
+        packet_restriction = int(parsed_json['Policy'][0]['UDP_packet_restriction'])
+        master_ip_list = list()
+        for grp in grp_name_list:
+            unicode_ip_list = parsed_json["Groups"][0][grp]["host_ips"]
+            ip_list = map(str, unicode_ip_list)
+            master_ip_list.append(ip_list)
+
+        print("master_IP_list", master_ip_list)
+
+        return master_ip_list, grp_name_list
+
+    def find_grp(self, master_ip_list, ip):
+        for sublist in master_ip_list:
+            # print("sublist", sublist)
+            if ip in sublist:
+                # print "Found it!", sublist
+                break
+        found_grp = master_ip_list.index(sublist)
+
+        return found_grp
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        "When a packet_in message is received policy is enforced"
+        print("Packet_In Message received")
+        # packet_restriction = 1
+        # group_restriction = 1
+        # G1 = ["10.10.1.1", "10.10.1.2"]
+        # G2 = ["10.10.1.3"]
+        master_ip_list, grp_name_list = self.jparsing()
+
+
+        msg = ev.msg
+
+        pkt = packet.Packet(msg.data)
+        arp_packet = pkt.get_protocols(arp.arp)
+        ipv4_packet = pkt.get_protocols(ipv4.ipv4)
+        udp_packet = pkt.get_protocols(udp.udp)
+
+        if udp_packet:
+            if packet_restriction == 1:
+                print("UDP packet detected. Access Denied")
+                return
+            else:
+                pass
+
+        if arp_packet:
+            print(arp_packet)
+            source_ip = arp_packet[0].src_ip
+            destination_ip = arp_packet[0].dst_ip
+        elif ipv4_packet:
+            source_ip = ipv4_packet[0].src
+            destination_ip = ipv4_packet[0].dst
+
+        src_g = str(grp_name_list[self.find_grp(master_ip_list, source_ip)])
+        dest_g = str(grp_name_list[self.find_grp(master_ip_list, destination_ip)])
+
+
+        # if source_ip in G1:
+        #     src_g = 1
+        # elif source_ip in G2:
+        #     src_g = 2
+        # else:
+        #     src_g = 0
+        #
+        # if destination_ip in G1:
+        #     dest_g = 1
+        # elif destination_ip in G2:
+        #     dest_g = 2
+        # else:
+        #     dest_g = 0
+
+        if group_restriction == 1:
+            if dest_g == src_g:
+                print("Allow Packet Transfer")
+                self.allow_access(pkt, msg)
+            else:
+                print("Deny the Access")
+                self.deny_access(pkt, msg)
+        else:
+            print("Allow Access")
+            self.allow_access(pkt, msg)
